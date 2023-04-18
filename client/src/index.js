e9b4be47807308706bc2234464a21eb5c5a276e4
betalantz <betalantz@gmail.com>
2023-04-18 10:54:38 -0700

    remove CI from environment

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 
-process.env.CI
-
 ReactDOM.render(
   <Router>
     <App />
